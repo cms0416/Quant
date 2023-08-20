f6edd189a01acebc4dbe533c4275411ca13c0320
cms0416 <cms0416@naver.com>
2023-08-20 22:25:05 +0900

    -

--- Script_R/네이버카페_스크래핑.R
+++ Script_R/네이버카페_스크래핑.R
@@ -1,11 +1,3 @@
-###############################################################################################################
-#
-# selenium 실행 : 윈도우에서 cmd를 통해 명령 프롬프트를 연 후, 아래 명령어를 입력(cmd창은 계속 열어둔다.)
-#
-# cd C:\Rselenium
-# java -Dwebdriver.gecko.driver="geckodriver.exe" -jar selenium-server-standalone-4.0.0-alpha-2.jar -port 4567
-#
-###############################################################################################################
 
 ################################################################################
 ## 라이브러리 로드
@@ -24,7 +16,7 @@ library(binman)
 library(webdriver)
 ################################################################################
 
-## 접속합 url 설정
+## 접속할 url 설정
 url <- "https://cafe.naver.com/infinitebuying"
 
 ## 크롬 드라이버 버전 확인
@@ -41,18 +33,6 @@ rD <- rsDriver(
 
 remDr <- rD$client
 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-ch = wdman::chrome(port=4567L)
-
-## 4567번 포트와 크롬 연결
-remDr <- remoteDriver(
-  remoteServerAddr = "localhost",
-  port = 4567L,
-  browserName = "chrome"
-)
-
-## 크롬 실행
-remDr$open()
 
 ## url 접속
 remDr$navigate(url)
@@ -60,14 +40,20 @@ remDr$navigate(url)
 ## 게시판 열기기
 remDr$findElement(using = "xpath", value = '/html/body/div[3]/div/div[6]/div[1]/div[2]/div[2]/ul[4]/li[1]/a')$clickElement()
 
-## 검색 창 클릭
-remDr$findElement(using = "xpath", value = '/html/body/div[1]/div/div[7]/form/div[3]/div/input')$clickElement()
-
-## 검색어 입력
-remDr$findElement(using = "xpath", value = '/html/body/div[1]/div/div[7]/form/div[3]/div/input/option[value() = "tqqq"]')$clickElement()
+## 게시판 검색어 입력
+remDr$findElement(using = 'class', 'input_component')$sendKeysToElement(list("TQQQ"))
 
 ## 검색 버튼 클릭
-remDr$findElement(using = "xpath", value = '/html/body/div[1]/div/div[7]/form/div[3]/button')$clickElement()
+remDr$findElement(using = 'class', 'btn-search-green')$clickElement()
+
+## 검색 창 클릭
+# remDr$findElement(using = "xpath", value = '/html/body/div[1]/div/div[8]/form/div[3]/div/input')$clickElement()
+
+## 검색어 입력
+# remDr$findElement(using = "xpath", value = '/html/body/div[1]/div/div[7]/form/div[3]/div/input/option[value() = "tqqq"]')
+
+## 검색 버튼 클릭
+# remDr$findElement(using = "xpath", value = '/html/body/div[1]/div/div[7]/form/div[3]/button')$clickElement()
 
 
 frontPage <- remDr$getPageSource()
@@ -80,3 +66,20 @@ frontPage <- remDr$getPageSource()
 
 ## 페이지 로딩 시간 대기
 Sys.sleep(1)
+
+
+
+
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ch = wdman::chrome(port=4567L)
+
+## 4567번 포트와 크롬 연결
+remDr <- remoteDriver(
+  remoteServerAddr = "localhost",
+  port = 4567L,
+  browserName = "chrome"
+)
+
+## 크롬 실행
+remDr$open()
